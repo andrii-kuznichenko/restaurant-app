e0592a9357373b269559ce35866ff09b1b19c1f4
Andrii <andriikuznichenko@gmail.com>
2024-01-26 11:25:07 +0100

    forbidden to create several tables with identical number for one restaurant

--- server/routes/table.js
+++ server/routes/table.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { authenticate } = require('../middleware/auth');
 const authRouter = express.Router();
 const { addTable, login, logout, getLoggedInTable } = require('../controllers/table');
-authRouter.post('/register', addTable);
+authRouter.post('/addTable', addTable);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
 authRouter.get('/currentTable', authenticate, getLoggedInTable);
