2737d67026d3393967b23e9a20d3308b2657ea1c
CiaranW12 <ciaran.walker12@gmail.com>
2024-01-31 10:06:37 +0100

    websocket integration

--- client/src/components/AdminMenu.jsx
+++ client/src/components/AdminMenu.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io(import.meta.env.VITE_SERVER_BASE_URL, {
+  transports: ["websocket"],
+});
 
 function AdminMenu() {
   const [menuItems, setMenuItems] = useState([]);
