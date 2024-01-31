59b71ec0ac48da78db041dfce0105eb4777e5e4e
CiaranW12 <ciaran.walker12@gmail.com>
2024-01-31 17:15:09 +0100

    Added Andrii's ideas, adjusted basic tab-style styling for CURRENT and HIDDEN meals. Removed FORM style.

--- client/src/components/AdminMenu.jsx
+++ client/src/components/AdminMenu.jsx
@@ -11,82 +11,137 @@ function AdminMenu() {
   const [menuItems, setMenuItems] = useState([]);
 
   useEffect(() => {
-
-    socket.emit("connectToMenu", {restaurantId: admin.restaurantId});
+    socket.emit("connectToMenu", { restaurantId: admin.restaurantId });
     socket.on(`getMenuAdmin-${admin.restaurantId}`, (receivedMenu) => {
       console.log(receivedMenu);
       setMenuItems(receivedMenu);
     });
-
   }, []);
 
-  const handleEdit = (index, prop, value) => {
-    // const updatedItems = [...menuItems];
-    // updatedItems[index][prop] = value;
-    // setMenuItems(updatedItems);
-    // fetch(`http://localhost:4000/${updatedItems[index]._id}`, {
-    //   method: "PUT",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(updatedItems[index]),
-    // });
-  };
+  const [tab, setTab] = useState("active");
 
   return (
-    // TO DO create categories, two tabs for shown meals and hidden meals 
     <div>
-      <h1>Admin Menu</h1>
-      {menuItems?.length !== 0? menuItems.menu.map((item, index) => (
-        <form key={index}>
-          <label>Title</label>
-          <input
-            defaultValue={item.title}
-            onChange={(e) => handleEdit(index, "title", e.target.value)}
-          />
-  
-          <label>Description</label>
-          <input
-            defaultValue={item.description}
-            onChange={(e) => handleEdit(index, "description", e.target.value)}
-          />
-  
-          <label>Allergens</label>
-          <input
-            defaultValue={item.allergens.join(", ")}
-            onChange={(e) =>
-              handleEdit(index, "allergens", e.target.value.split(", "))
-            }
-          />
-  
-          <label>Price</label>
-          <input
-            defaultValue={item.price}
-            onChange={(e) => handleEdit(index, "price", e.target.value)}
-          />
-  
-          <label>Image</label>
-          <input
-            defaultValue={item.image}
-            onChange={(e) => handleEdit(index, "image", e.target.value)}
-          />
-  
-          <label>Hide</label>
-          <input
-            type="checkbox"
-            defaultChecked={item.hide}
-            onChange={(e) => handleEdit(index, "hide", e.target.checked)}
-          />
-  
-          <label>Category</label>
-          <input
-            defaultValue={item.category}
-            onChange={(e) => handleEdit(index, "category", e.target.value)}
-          />
-        </form>
-      )): <div>Loading..</div>}
+      <h1 className="text-2xl font-bold mb-4">Admin Menu</h1>
+      <div>
+        <button className="mr-4" onClick={() => setTab("active")}>
+          Active Meals
+        </button>
+        <button onClick={() => setTab("hidden")}>Hidden Meals</button>
+      </div>
+      {tab === "active"
+        ? menuItems?.length !== 0 &&
+          menuItems.menu
+            .filter((item) => !item.hide)
+            .map((item, index) => (
+              <div
+                key={index}
+                className="mb-4 p-4 border border-gray-300 rounded"
+              >
+                <label className="block text-sm font-bold mb-2">Title</label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  defaultValue={item.title}
+                  onChange={(e) => handleEdit(index, "title", e.target.value)}
+                />
+
+                <label className="block text-sm font-bold mb-2 mt-4">
+                  Price
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  defaultValue={item.price}
+                  onChange={(e) => handleEdit(index, "price", e.target.value)}
+                />
+
+                <label className="block text-sm font-bold mb-2 mt-4">
+                  Image
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  defaultValue={item.image}
+                  onChange={(e) => handleEdit(index, "image", e.target.value)}
+                />
+
+                <label className="block text-sm font-bold mb-2 mt-4">
+                  Hide
+                </label>
+                <input
+                  type="checkbox"
+                  className="mr-2 leading-tight"
+                  defaultChecked={item.hide}
+                  onChange={(e) => handleEdit(index, "hide", e.target.checked)}
+                />
+
+                <label className="block text-sm font-bold mb-2 mt-4">
+                  Category
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  defaultValue={item.category}
+                  onChange={(e) =>
+                    handleEdit(index, "category", e.target.value)
+                  }
+                />
+              </div>
+            ))
+        : menuItems?.length !== 0 &&
+          menuItems.menu
+            .filter((item) => item.hide)
+            .map((item, index) => (
+              <div
+                key={index}
+                className="mb-4 p-4 border border-gray-300 rounded"
+              >
+                <label className="block text-sm font-bold mb-2">Title</label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  defaultValue={item.title}
+                  onChange={(e) => handleEdit(index, "title", e.target.value)}
+                />
+
+                <label className="block text-sm font-bold mb-2 mt-4">
+                  Price
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  defaultValue={item.price}
+                  onChange={(e) => handleEdit(index, "price", e.target.value)}
+                />
+
+                <label className="block text-sm font-bold mb-2 mt-4">
+                  Image
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  defaultValue={item.image}
+                  onChange={(e) => handleEdit(index, "image", e.target.value)}
+                />
+
+                <label className="block text-sm font-bold mb-2 mt-4">
+                  Hide
+                </label>
+                <input
+                  type="checkbox"
+                  className="mr-2 leading-tight"
+                  defaultChecked={item.hide}
+                  onChange={(e) => handleEdit(index, "hide", e.target.checked)}
+                />
+
+                <label className="block text-sm font-bold mb-2 mt-4">
+                  Category
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  defaultValue={item.category}
+                  onChange={(e) =>
+                    handleEdit(index, "category", e.target.value)
+                  }
+                />
+              </div>
+            ))}
     </div>
   );
-          }
-          
+}
+
 export default AdminMenu;
