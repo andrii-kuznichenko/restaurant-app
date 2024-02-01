b9f2cf6222866401150293bbe4d3e5c99cdeda0f
Andrii <andriikuznichenko@gmail.com>
2024-02-01 09:38:17 +0100

    admin menu

--- client/src/components/UserMenu.jsx
+++ client/src/components/UserMenu.jsx
@@ -58,9 +58,12 @@ const SendOrder = () => {
     <>
     <div>UserMenu</div>
     {menu?.length !== 0?menu.menu.map(meal => (
+      <>
       <h1>
         {meal.title}
       </h1>
+      <img src={meal.image} alt="" />
+      </>
     )):<p></p>}
     <button type="button" 
     className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
