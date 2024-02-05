182f631da7eb23d2df5964473f229fc7a5ffe657
Valentijn <v.f.vermeer@gmail.com>
2024-02-05 13:51:18 +0100

    changed the styling of the order overview page

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,12 +2,13 @@ import { useContext } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { AuthContext } from '../context/Auth';
 import { AuthTableContext } from '../context/AuthTable';
+import {DefaultSidebar } from "./DefaultSidebar"
 
 function Protected() {
   const { admin, loading } = useContext(AuthContext);
   const { table, loadingTable } = useContext(AuthTableContext);
 
-  return <>{!loading && <>{admin ? <Outlet /> : !loadingTable && table ? <Navigate to="/user" /> : <Navigate to="/login"/>}</>}</>;
+  return <>{!loading && <>{admin ?  <Outlet />: !loadingTable && table ? <Navigate to="/user" /> : <Navigate to="/login"/>}</>}</>;
 }
 
 export default Protected;
