3ad3455394858c75020a7698ddd5ba6860b8d19e
CiaranW12 <ciaran.walker12@gmail.com>
2024-02-05 13:32:38 +0100

    added delete/edit BUT i have problems with backend functionality

--- client/src/components/AdminMenu.jsx
+++ client/src/components/AdminMenu.jsx
@@ -5,6 +5,7 @@ import CreateQrCode from "./CreateQrCode";
 import AdminTables from "./AdminTables";
 import { useNavigate } from "react-router-dom";
 import AdminMenuModal from "./AdminMenuModal";
+import axios from "axios";
 
 const socket = io(import.meta.env.VITE_SERVER_BASE_URL, {
   transports: ["websocket"],
@@ -48,10 +49,47 @@ function AdminMenu() {
       setMenuItems(receivedMenu);
     });
   }, []);
+  const handleUpdate = async (item, value) => {
+    const updateMeal = {
+      restaurantId: admin.restaurantId,
+      mealId: item._id,
+      newValue: value,
+      operation: "update",
+    };
 
+    try {
+      await axios.put(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/${item._id}`,
+        updateMeal
+      );
+      socket.emit("updateMenu", updateMeal);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDelete = async (item) => {
+    const deleteMeal = {
+      restaurantId: admin.restaurantId,
+      mealId: item._id,
+      operation: "delete",
+    };
+
+    try {
+      await axios.delete(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/${item._id}`,
+        { data: deleteMeal }
+      );
+      socket.emit("deleteFromMenu", deleteMeal);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="bg-yellow-200 flex flex-col items-center">
-      <h1 className="text-2xl font-bold mb-4">Admin Menu</h1>
+      <h1 className="text-2xl mb-4 bg-black text-white rounded-lg p-2">
+        Active and OOS menu items
+      </h1>
       <div className="flex border-b">
         <button
           className={`py-2 px-4 bg-white border-l border-t border-r rounded-t ${
@@ -84,10 +122,10 @@ function AdminMenu() {
               className="w-full md:w-1/2 lg:w-1/2 xl:w-1/3 mx-auto bg-white rounded-xl shadow-md overflow-hidden m-3"
             >
               <div className="p-8">
-                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold text-center">
+                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold text-center pb-2">
                   {item.category}
                 </div>
-                <div className="md:flex-shrink-0">
+                <div className="md:flex-shrink-0 pb-2">
                   <img
                     className="h-48 w-full object-cover rounded-lg"
                     src={item.image}
@@ -100,7 +138,7 @@ function AdminMenu() {
                 >
                   {item.title}
                 </a>
-                <p className="mt-2 text-gray-500">{item.description}</p>
+                <p className="mt-2 text-gray-600">{item.description}</p>
                 <div className="mt-4 flex justify-between">
                   <button
                     onClick={() => handleEdit(item, !item.hide)}
@@ -110,9 +148,19 @@ function AdminMenu() {
                         : "bg-red-400 hover:bg-gray-400 text-black-700"
                     }`}
                   >
-                    {item.hide
-                      ? "Meal Hidden from Menu"
-                      : "Remove from menu"}
+                    {item.hide ? "Meal Hidden from Menu" : "Remove from menu"}
+                  </button>
+                  <button
+                    onClick={() => handleUpdate(item, !item.hide)} // assuming you want to use the same function for editing
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => handleDelete(item)} // assuming you want to use the same function for deleting
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Delete
                   </button>
                   <button
                     onClick={() => openModal(item)}
