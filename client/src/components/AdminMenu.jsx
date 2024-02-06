f84dab22952e2f83e2afce03dfc6d83123ee7d31
CiaranW12 <ciaran.walker12@gmail.com>
2024-02-06 17:36:47 +0100

    changed to a grid layout

--- client/src/components/AdminMenu.jsx
+++ client/src/components/AdminMenu.jsx
@@ -52,10 +52,7 @@ function AdminMenu() {
   }, []);
 
   return (
-    <div className="bg-grey-200 flex flex-col items-center">
-      <h1 className="text-2xl mb-4 bg-black text-white rounded-lg p-2">
-        Active and OOS menu items
-      </h1>
+    <div className="mx-auto m-3 mt-16">
       <div className="flex border-b">
         <button
           className={`text-black py-2 px-4 bg-grey-200 border-l border-t border-r rounded-t ${
@@ -68,7 +65,7 @@ function AdminMenu() {
           Active Meals
         </button>
         <button
-          className={`text-black py-2 px-4 bg-grey-200 border-l border-t border-r rounded-t ${
+          className={`text-black py-3 px-4 bg-grey-200 border-l border-t border-r rounded-t ${
             tab === "hidden"
               ? "text-black border-green-500 bg-green-200"
               : "text-gray-500"
@@ -78,108 +75,109 @@ function AdminMenu() {
           Off the menu
         </button>
       </div>
-
-      {tab === "active" &&
-        menuItems.menu
-          ?.filter((item) => !item.hide)
-          ?.map((item, index) => (
-            <div
-              key={index}
-              className="w-full md:w-1/2 lg:w-1/2 xl:w-1/3 mx-auto bg-green-100 rounded-xl shadow-md overflow-hidden m-3"
-            >
-              <div className="p-8">
-                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold text-center pb-2">
-                  {item.category}
-                </div>
-                <div className="md:flex-shrink-0 pb-2">
-                  <img
-                    className="h-48 w-full object-cover rounded-lg"
-                    src={item.image}
-                    alt={item.title}
-                  />
-                </div>
-                <a
-                  href="#"
-                  className="block mt-1 text-lg leading-tight font-medium text-black hover:underline text-center"
-                >
-                  {item.title}
-                </a>
-                <p className="mt-2 text-gray-600">{item.description}</p>
-                <div className="mt-4 flex justify-between">
-                  <button
-                    onClick={() => handleEdit(item, !item.hide)}
-                    className={`cursor-pointer px-4 py-2 rounded-full shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 ${
-                      item.hide
-                        ? "bg-green-200 hover:bg-green-300 text-black"
-                        : "bg-red-200 hover:bg-red-300 text-black"
-                    }`}
+      <div className="mx-auto m-3 md:grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {tab === "active" &&
+          menuItems.menu
+            ?.filter((item) => !item.hide)
+            ?.map((item, index) => (
+              <div
+                key={index}
+                className="mx-auto bg-green-100 rounded-xl shadow-md overflow-hidden m-3 md:grid flex flex-col"
+              >
+                <div className="p-8">
+                  <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold text-center pb-2">
+                    {item.category}
+                  </div>
+                  <div className="md:flex-shrink-0 pb-2">
+                    <img
+                      className="h-48 w-full object-cover rounded-lg"
+                      src={item.image}
+                      alt={item.title}
+                    />
+                  </div>
+                  <a
+                    href="#"
+                    className="block mt-1 text-lg leading-tight font-medium text-black hover:underline text-center"
                   >
-                    {item.hide ? "Meal Hidden from Menu" : "Remove from menu"}
-                  </button>
-                  <button
-                    onClick={() => openModal(item)}
-                    className="bg-green-200 hover:bg-green-300 text-black font-bold py-2 px-4 rounded-full shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
-                  >
-                    Meal Details
-                  </button>
+                    {item.title}
+                  </a>
+                  <p className="mt-2 text-gray-600">{item.description}</p>
+                  <div className="mt-4 flex justify-between">
+                    <button
+                      onClick={() => handleEdit(item, !item.hide)}
+                      className={`cursor-pointer px-4 py-2 rounded-full shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 ${
+                        item.hide
+                          ? "bg-green-200 hover:bg-green-300 text-black"
+                          : "bg-red-200 hover:bg-red-300 text-black"
+                      }`}
+                    >
+                      {item.hide ? "Meal Hidden from Menu" : "Remove from menu"}
+                    </button>
+                    <button
+                      onClick={() => openModal(item)}
+                      className="bg-green-200 hover:bg-green-300 text-black font-bold py-2 px-4 rounded-full shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+                    >
+                      Meal Details
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
 
-      <AdminMenuModal
-        item={selectedItem}
-        isOpen={isModalOpen}
-        closeModal={closeModal}
-      />
+        <AdminMenuModal
+          item={selectedItem}
+          isOpen={isModalOpen}
+          closeModal={closeModal}
+        />
 
-      {tab === "hidden" &&
-        menuItems.menu
-          .filter((item) => item.hide)
-          .map((item, index) => (
-            <div
-              key={index}
-              className="w-full md:w-1/2 lg:w-1/2 xl:w-1/3 mx-auto bg-green-100 rounded-xl shadow-md overflow-hidden m-3"
-            >
-              <div className="p-8">
-                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold text-center">
-                  {item.category}
-                </div>
-                <div className="md:flex-shrink-0">
-                  <img
-                    className="h-48 w-full object-cover rounded-lg"
-                    src={item.image}
-                    alt={item.title}
-                  />
-                </div>
-                <a
-                  href="#"
-                  className="block mt-1 text-lg leading-tight font-medium text-black hover:underline text-center"
-                >
-                  {item.title}
-                </a>
-                <p className="mt-2 text-gray-500">{item.description}</p>
-                <div className="mt-4 flex justify-between">
-                  <button
-                    onClick={() => handleEdit(item, !item.hide)}
-                    className={`mr-2 leading-tight font-bold py-2 px-4 rounded-full shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 ${
-                      item.hide
-                        ? "bg-blue-200 hover:bg-blue-300 text-black"
-                        : "bg-gray-200 hover:bg-gray-300 text-black"
-                    }`}
+        {tab === "hidden" &&
+          menuItems.menu
+            .filter((item) => item.hide)
+            .map((item, index) => (
+              <div
+                key={index}
+                className="mx-auto bg-green-100 rounded-xl shadow-md overflow-hidden m-3 md:grid flex flex-col"
+              >
+                <div className="p-8">
+                  <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold text-center pb-2">
+                    {item.category}
+                  </div>
+                  <div className="md:flex-shrink-0 pb-2">
+                    <img
+                      className="h-48 w-full object-cover rounded-lg"
+                      src={item.image}
+                      alt={item.title}
+                    />
+                  </div>
+                  <a
+                    href="#"
+                    className="block mt-1 text-lg leading-tight font-medium text-black hover:underline text-center"
                   >
-                    {item.hide ? "Put back on menu" : "Hide"}
-                  </button>
-                  <button
-                    onClick={() => openModal(item)}
-                    className="bg-green-200 hover:bg-green-300 text-black font-bold py-2 px-4 rounded-full shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
-                  >
-                    Meal Details
-                  </button>
+                    {item.title}
+                  </a>
+                  <p className="mt-2 text-gray-600">{item.description}</p>
+                  <div className="mt-4 flex justify-between">
+                    <button
+                      onClick={() => handleEdit(item, !item.hide)}
+                      className={`cursor-pointer px-4 py-2 rounded-full shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 ${
+                        item.hide
+                          ? "bg-green-200 hover:bg-green-300 text-black"
+                          : "bg-red-200 hover:bg-red-300 text-black"
+                      }`}
+                    >
+                      {item.hide ? "Put back on menu" : "Hide"}
+                    </button>
+                    <button
+                      onClick={() => openModal(item)}
+                      className="bg-green-200 hover:bg-green-300 text-black font-bold py-2 px-4 rounded-full shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+                    >
+                      Meal Details
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+      </div>
     </div>
   );
 }
