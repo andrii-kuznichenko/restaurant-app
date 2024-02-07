6948ca3f2bfd84c825f34e45416ff4e8a148ec86
SabinaMB <sabina.marian.blaariu@gmail.com>
2024-02-07 12:09:00 +0100

    2

--- client/src/components/UserMenu.jsx
+++ client/src/components/UserMenu.jsx
@@ -97,48 +97,54 @@ const getTotalPrice = (id)=>{
       categories.map(category => (
         <Accordion key={category}>
         <Accordion.Panel>
-        <Accordion.Title>{category}</Accordion.Title>
+        <Accordion.Title className><span className='font-Poppins font-bold text-center inline-block w-full'>{category}</span></Accordion.Title>
         <Accordion.Content>
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         {userMenu.menu.map(item => {
           if(item.category === category){
             return(
-              <tr key={item._id} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <td class="p-4">
-                  <img src={item.image} class="w-16 md:w-32 max-w-full max-h-full" alt={item.title}/>
+              <tr key={item._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+             
+             {/*Meal image, meal title*/}
+              <td className="p-4">
+                  <img src={item.image} className="w-16 md:w-32 max-w-full max-h-full" alt={item.title}/>
               </td>
-              <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+              <td className="px-6 py-4 font-Poppins font-semibold text-gray-900 dark:text-white">
                   {item.title}
               </td>
-              <td class="px-6 py-4">
-                  <div class="flex items-center">
-                      <button class="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" 
+
+              {/*Add quantity*/}
+              <td className="px-6 py-4">
+                  <div className="flex items-center">
+                      <button className="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" 
                       type="button"
                       onClick={() => handleRemove(item)}>
-                          <span class="sr-only">Quantity button</span>
-                          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                          <span className="sr-only">Quantity button</span>
+                          <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
                           </svg>
                       </button>
                       <div>
-                          <p className="bg-gray-50 w-14 border text-center border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                          <p classNameName="bg-gray-50 w-14 border text-center border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             {getQuantity(item._id)}</p>
 
                          
                       </div>
-                      <button class="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" 
+                      <button className="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" 
                       type="button"
                       onClick={() => handleAdd(item)}>
-                          <span class="sr-only">Quantity button</span>
-                          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                          <span className="sr-only">Quantity button</span>
+                          <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
                           </svg>
                       </button>
                       
                   </div>
               </td>
-              <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+
+              {/*Meal price; total price*/}
+              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
                   {item.price} Euro
                   <p className="text-xs text-gray-500 dark:text-gray-400">Total: {getTotalPrice(item._id)} Euro</p>
               </td>
