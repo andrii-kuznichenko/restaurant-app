e3dc077094ea4ff5ad7bd4d3877c9218478e899c
Valentijn <v.f.vermeer@gmail.com>
2024-02-12 21:41:12 +0100

    save

--- client/src/components/MyRestaurant.jsx
+++ client/src/components/MyRestaurant.jsx
@@ -770,7 +770,7 @@ const MyRestaurant = () => {
                     className="text-white bg-footerBackground bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                     type="submit"
                   >
-                    Save all
+                    Save
                   </button>
                 </div>
               </div>
